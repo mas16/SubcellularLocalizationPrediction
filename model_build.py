22b251c93e9b6e962f7799e648d4b2d644ccc2e7
mas16 <mattstetz@gmail.com>
2019-06-16 10:54:36 -0400

    clean up

--- model_build.py
+++ model_build.py
@@ -8,35 +8,30 @@ by MAS 06.2019
 """
 
 import pandas as pd
-import seaborn as sns
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import GridSearchCV
-import xgboost as xgb
 
+# Set random seed
 seed = 1234
 
+# Read pandas dataframe
 df = pd.read_csv("./ecoli_proteome_features.csv")
 
 # Final data set shape
-print(df.shape)
+print("Shape of initial DF: ", df.shape)
 
 # Number of features
-print(len(list(df)[3:]))
+print("Number of Features: ", len(list(df)[3:]))
 
 # We have a total of ~ 2000 observations.
 # We should make sure our features are ~10x less than number of observations.
@@ -47,24 +42,22 @@ df = df.drop(["U", "X", "A"], axis=1)
 # Drop colinear features with abs(r) > 0.5
 df = df.drop(["ss_mean", "hydro_mean"], axis=1)
 
-## Number of soluble proteins
-print("soluble:" , df[df["Local"]==0].shape)
+# Number of soluble proteins
+print("soluble: ", df[df["Local"] == 0].shape)
 
-## Number of membrane proteins
-print("membrane:" , df[df["Local"]==1].shape)
+# Number of membrane proteins
+print("membrane: ", df[df["Local"] == 1].shape)
 
-## Little bit of a class imbalance
-df = shuffle(df, random_state=seed)
-balance_membrane_df = df[df["Local"]==1].iloc[0:874,]
-balance_soluble_df = df[df["Local"]==0]
-df = pd.concat([balance_membrane_df, balance_soluble_df])
-
-##############
+# Subsample to correct for class imbalance
+# df = shuffle(df, random_state=seed)
+# balance_membrane_df = df[df["Local"]==1].iloc[0:874,]
+# balance_soluble_df = df[df["Local"]==0]
+# df = pd.concat([balance_membrane_df, balance_soluble_df])
 
 # Final number of features is now 10x less than number of observations
-print(len(list(df)[3:]))
+print("Final number of features: ", len(list(df)[3:]))
 
-## Ok Let's start testing some models
+# Ok Let's start testing some models
 # Shuffle data
 df = shuffle(df, random_state=seed)
 
@@ -83,33 +76,33 @@ Y = Y.astype("int")
 test_size = 0.40
 
 # Test options and evaluation metric
-scoring = 'accuracy'
+scoring = "accuracy"
 
 # Set up train and validation sets
 X_train, X_test, Y_train, Y_test = \
     model_selection.train_test_split(X, Y, test_size=test_size,
                                      random_state=seed)
-
 # Standardize the data
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-
 # Try different models
-models = []
-models.append(('LR', LogisticRegression(solver="lbfgs")))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(("RF", RandomForestClassifier(n_estimators=100, random_state=seed)))
-models.append(("GB", GradientBoostingClassifier(n_estimators=100, random_state=seed)))
-models.append(('SVM', SVC(gamma="scale")))
-#models.append(("XGBRF", xgb.XGBRFRegressor(random_state=seed)))
+# Arguments that are passed to different model function calls are the DEFAULT
+# arguments recommended by the scikit-learn documentation
+models = [('LR', LogisticRegression(solver="lbfgs")),
+          ('CART', DecisionTreeClassifier()),
+          ("RF", RandomForestClassifier(n_estimators=100, random_state=seed)),
+          ("RF", RandomForestClassifier(n_estimators=100, random_state=seed)),
+          ("GB", GradientBoostingClassifier(n_estimators=100,
+                                            random_state=seed)),
+          ('SVM', SVC(gamma="scale"))]
 
 # Evaluate each model in turn
 results = []
 names = []
 for name, model in models:
-    # 5x cross validation
+    # 5x cross validation - Default value recommended by scikit-learn
     kfold = model_selection.KFold(n_splits=5, random_state=seed)
     cv_results = model_selection.cross_val_score(model, X_train, Y_train,
                                                  cv=kfold, scoring=scoring)
@@ -117,9 +110,7 @@ for name, model in models:
     names.append(name)
     print(name, cv_results.mean(), cv_results.std())
 
-
-
-# SVM parameter tuning using 5 fold cross validation
+# SVM parameter tuning using 5x cross validation
 kernels = ["rbf", "linear", "poly", "sigmoid"]
 cv_results = []
 results = []
@@ -133,17 +124,20 @@ for kernel in kernels:
     c = grid_search.best_params_["C"]
     gamma = grid_search.best_params_["gamma"]
     kfold = model_selection.KFold(n_splits=5, random_state=seed)
-    cv_results = model_selection.cross_val_score(SVC(kernel=kernel, C=c, gamma=gamma), X_train, Y_train, cv=kfold, scoring=scoring)
+    cv_results = model_selection.cross_val_score(SVC(kernel=kernel,
+                                                     C=c, gamma=gamma),
+                                                 X_train, Y_train, cv=kfold,
+                                                 scoring=scoring)
     results.append(cv_results)
     print(kernel)
     print(grid_search.best_params_)
     print(cv_results.mean(), cv_results.std())
 
-
 # Apply final model to validation set
 classifier = SVC(kernel="rbf", C=5, gamma=0.1)
 classifier.fit(X_train, Y_train)
-Y_pred = classifier.predict(X_test)
-print(confusion_matrix(Y_test,Y_pred))
-print(classification_report(Y_test,Y_pred))
-print(accuracy_score(Y_test, Y_pred))
+Y_prediction = classifier.predict(X_test)
+print("Confustion Matrix: ")
+print(confusion_matrix(Y_test, Y_prediction))
+print(classification_report(Y_test, Y_prediction))
+print("Accuracy: ", accuracy_score(Y_test, Y_prediction))
